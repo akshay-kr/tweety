2e6651dfb6bc4e40edf86327c458a4af3aea4d0e
Akshay Kumar <akshay@Abhinavs-MacBook-Pro.local>
2017-01-06 00:37:46 +0530

    created timeline page

--- public/scripts/controllers/timeline.js
+++ public/scripts/controllers/timeline.js
@@ -17,8 +17,28 @@ angular.module('tweetyApp')
 				});
 		}
 
+		$scope.formatDate = function (date) {
+			return new Date(date);
+		};
+
 		$scope.format = function (count) {
 			return Math.abs(Number(count)) >= 1.0e+9 ? Math.abs(Number(count)) / 1.0e+9 + "B" : Math.abs(Number(count)) >= 1.0e+6 ? (Math.abs(Number(count)) / 1.0e+6).toFixed(1) + "M" : Math.abs(Number(count)) >= 1.0e+3 ? (Math.abs(Number(count)) / 1.0e+3).toFixed(1) + "K" : Math.abs(Number(count));
+		};
+
+		$scope.formatMsg = function (text, entities) {
+			var hashtags = entities.hashtags;
+			if (hashtags.length > 0) {
+				hashtags.forEach(function (hashtag) {
+					text = text.replace('#' + hashtag.text, '<span class="hash">#' + hashtag.text + '</span>');
+				});
+			}
+			return text;
+		};
+		$scope.parseImageUrl = function (url) {
+			if (url) {
+				url = url.replace("_normal", "");
+				return url;
+			}
 		};
 		getInfo(screenName);
 	});
